2a1b4e684b2185626e7b3df2cbe1280a60ab0d2d
Mira Violet <m@amethyst.design>
2023-07-01 17:43:06 -0400

    fix: importing fonts correctly

--- theme/header.php
+++ theme/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -10,22 +11,27 @@
  * @package OpenPhone
  */
 
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-<div id="page">
-	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'openphone' ); ?></a>
+	<div id="page">
+		<a href="#content" class="sr-only"><?php esc_html_e('Skip to content', 'openphone'); ?></a>
 
-	<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
+		<?php get_template_part('template-parts/layout/header', 'content'); ?>
 
-	<div id="content">
+		<div id="content">
