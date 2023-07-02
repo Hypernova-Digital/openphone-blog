45f715965504eac33667915c984174439a1700aa
Mira Violet <m@amethyst.design>
2023-07-01 20:49:12 -0400

    feat: adding replated posts to bottom of post

--- theme/inc/template-tags.php
+++ theme/inc/template-tags.php
@@ -81,7 +81,7 @@ if (!function_exists('openphone_entry_meta_categories')) :
 endif;
 
 if (!function_exists('openphone_entry_meta_tags')) :
-	
+
 	function openphone_entry_meta_tags()
 	{
 		$tags_list = get_the_tag_list('', __(', ', 'openphone'));
@@ -200,6 +200,43 @@ if (!function_exists('openphone_entry_footer')) :
 				get_the_title()
 			)
 		);
+		?>
+		<div class="related-posts-after-content">
+
+			<h3>You Might Also Like</h3>
+
+			<?php
+			global $post;
+			$orig_post = $post;
+			$tags = wp_get_post_tags($post->ID);
+			if ($tags) {
+				$tag_ids = array();
+				foreach ($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+				$args = array(
+					'tag__in' => $tag_ids,
+					'post__not_in' => array($post->ID),
+					'posts_per_page' => 4, // Number of related posts to display.
+					'caller_get_posts' => 1
+				);
+
+				$my_query = new wp_query($args);
+				while ($my_query->have_posts()) {
+					$my_query->the_post();
+			?>
+
+					<div class="related-thumb">
+						<a rel="external" href="<? the_permalink() ?>"><?php the_post_thumbnail(array(150, 100)); ?><br />
+							<?php the_title(); ?>
+						</a>
+
+					</div>
+			<?php }
+			}
+			$post = $orig_post;
+			wp_reset_query();
+			?>
+		</div>
+		<?php
 	}
 endif;
 
@@ -215,7 +252,7 @@ if (!function_exists('openphone_post_thumbnail')) :
 		}
 
 		if (is_singular()) :
-?>
+		?>
 
 			<figure>
 				<?php the_post_thumbnail(); ?>
