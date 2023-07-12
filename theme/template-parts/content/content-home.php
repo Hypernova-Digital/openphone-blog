a8a1ef2e76598f69121803a947ec2c09c7d35c90
Mira Violet <m@amethyst.design>
2023-07-11 21:01:39 -0400

    feat: 50/50 block styles and featured post styles

---
+++ theme/template-parts/content/content-home.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template part for displaying the home page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package OpenPhone
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+		<?php
+		// if ( ! is_front_page() ) {
+		// 	the_title( '<h1 class="entry-title">', '</h1>' );
+		// } else {
+		// 	the_title( '<h2 class="entry-title">', '</h2>' );
+		// }
+		?>
+	</header><!-- .entry-header -->
+
+	<?php openphone_post_thumbnail(); ?>
+
+	<div <?php openphone_content_class( 'entry-content' ); ?>>
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before' => '<div>' . __( 'Pages:', 'openphone' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
