c16b33a2f425656f83886270ae424e8eb234fc97
Mira Violet <m@amethyst.design>
2023-07-28 13:27:07 -0400

    fix: updating footer font

--- theme/build/openphone-latest-post/index.asset.php
+++ theme/build/openphone-latest-post/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '9fe884ff0a836a26df72');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '0d011270617111735640');
